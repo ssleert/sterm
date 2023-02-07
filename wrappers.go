a4eb376ab22c11442430ca73200324ae65c4b056
ssleert <ssleert@sfome.my.domain>
2023-02-07 12:08:16 +0300

    Fix: wrappers

--- wrappers.go
+++ wrappers.go
@@ -4,17 +4,20 @@ import (
 	"golang.org/x/term"
 )
 
+type State *term.State
+
 func Size() (x, y int, err error) {
 	x, y, err = term.GetSize(0)
 	return
 }
 
-func GetState() (s *term.State, err error) {
-	s, err = term.GetState(0)
+func GetState() (s State, err error) {
+	b, err := term.GetState(0)
+	s = State(b)
 	return
 }
 
-func Restore(s *term.State) (err error) {
+func Restore(s State) (err error) {
 	err = term.Restore(9, s)
 	return
 }
